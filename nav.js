ee1651f85be3e78be12b3ba9d4d71f3d46e810f1
ogiamien emmanuel tammy <ogiamien222@gmail.com>
2022-02-11 23:14:23 +0100

    made changes in the navbar, resized the images in the gallery, team and testimonial section and coded an on scroll feature, also reshaped and restyled the structure of the header and navbar

--- nav.js
+++ nav.js
@@ -1,14 +1,40 @@
+// side bar navigation menu
+
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle")
 
-navToggle.addEventListener("click", () => {
- const visibility = primaryNav.getAttribute("data-visible") 
+navToggle.addEventListener("click", toggleNav);
 
- if(visibility === "false") {
-     primaryNav.setAttribute("data-visible", true);
-     navToggle.setAttribute('aria-expanded', true);
- } else if (visibility === "true") {
-     primaryNav.setAttribute("data-visible", false);
-     navToggle.setAttribute('aria-expanded', false);
- } 
-});
+function toggleNav(){
+    const visibility = primaryNav.getAttribute("data-visible") 
+    // console.log(10)
+
+    if(visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+}
+
+const primaryMenu = document.querySelector(".on-click");
+primaryMenu.addEventListener("click", toggleNav);
+
+
+
+  // smooth scrolling
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        }
+      )
+    }
+  })
+  
